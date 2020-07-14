d395737eec73b292596b125f863f63f14f49a17f
Roy He <royhe62@yahoo.ca>
2020-07-13 22:06:45 -0700

    Added shift method and capability

--- Enigma.py
+++ Enigma.py
@@ -15,6 +15,7 @@ TODO: Explain Engima
 
 class Enigma:
     rotors = [None,None,None]
+    secondCritical = False
     #pbSettings are a string where 0 and 1 are switched, 2 and 3 are switched, and so forth
     #Rotor order is a 3 digit number in string form saying which rotor should go where.
     #Rotor settings is an array of integers saying the orientations of the rotors
@@ -39,10 +40,21 @@ class Enigma:
                 self.rotors[i] = RotorFive(rOrient)
             else:
                 raise ValueError(rSelection + " is not a valid Rotor")
+        
+        
+        self.secondCritical = self.rotors[1].orientation == self.rotors[1].turnkey - 1  #check if the second rotor will make it's turkey
+
+        if(self.secondCritical and self.rotors[2].orientation == self.rotors[2].turnkey - 1):   #if both the third and second are about to make their
+            self.secondCritical = False                                                         #turnkeys, then we must prevent the second rotor from
+            self.rotors[0].shift()                                                              #turning twice (by setting secondCritical to false) and turn the first rotor (since we turn before running anyway, this is the same difference)
     
     def scramble(self, message):
-        scrambled = self.cleanString(message.upper())
+        cleaned = self.cleanString(message.upper())
+        scrambled = ""
         
+        for x in cleaned:
+            scrambled += x
+
         return scrambled
     
     def cleanString(self, message):
@@ -53,6 +65,44 @@ class Enigma:
                 cleaned += x
     
         return cleaned
+    
+
+    """
+    Before a letter is passed through the Enigma machine, the rotors are shifted.
+
+    Signals go from the third Rotor to the First Rotor, and the shift is no different
+
+    First, we shift the third rotor, no matter what.
+
+    If the third rotor made it's "turnkey" (like Q to R for RotorOne), shift the second Rotor as well.
+
+    That's all simple, but the "second critical" part will give nightmares.
+
+    If the second rotor gets shifted onto the first part of it's turnkey(Q for Rotor One), then
+    next shift, the first and second rotors will also shift. We mark this by setting "second critical" to True
+
+    Rotors will also only shift once, so if the third rotor makes it's turnkey while the second not turn twice.
+    In this situation, all 3 rotors will switch once, just like a regular second critical.
+    
+    Fortunately, this can only happen right at the begining. I can avoid having to check if secondCritical is true everytime
+    I go turn the first Rotor, I'll simply check to see if this special ocasion rises as I initiate.
+
+    If it does, I just shift the first rotor and set second critical to false. Since the rotors are shifted before the character
+    is encrypted, it won't matter that the first rotor is different from how it's meant to be before the first run, and will 
+    be where they need to be after the first shift
+    """
+    def shiftRotors(self):
+        if(self.rotors[2].shift()):
+            self.rotors[1].shift()
+        
+        if(self.secondCritical):
+            self.rotors[0].shift()
+            self.rotors[1].shift()
+            self.secondCritical = False
+        
+        if(self.rotors[1].orientation == self.rotors[1].turnkey - 1):
+            self.secondCritical = True
+
 
     def __str__(self):
         rs = self.rotors[0].__str__()
@@ -131,10 +181,10 @@ class Rotor:
         self.type = type
     
     def shift(self):
-        if (self.orientation is 26):
-            self.orientation = self.orientation
+        if (self.orientation is 25):
+            self.orientation = 0
         else:
-            print(12)
+            self.orientation += 1
         
         return self.orientation == self.turnkey
 
@@ -171,35 +221,41 @@ class RotorOne(Rotor):
 class RotorTwo(Rotor):
     def __init__(self, orientation):
         super().__init__(orientation, 2)
-        self.turnkey = 6 #turns after going from E to F
+        self.turnkey = 5 #turns after going from E to F
         self.forwardWires = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
         self.backwardWires = "AJPCZWRLFBDKOTYUQGENHXMIVS"
 
 class RotorThree(Rotor):
     def __init__(self, orientation):
         super().__init__(orientation, 3)
-        self.turnkey = 23   #turns after going from V to W
+        self.turnkey = 22   #turns after going from V to W
         self.forwardWires = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
         self.backwardWires = "TAGBPCSDQEUFVNZHYIXJWLRKOM"
 
 class RotorFour(Rotor):
     def __init__(self, orientation):
         super().__init__(orientation, 4)
-        self.turnkey = 11 #turns after going from J to K
+        self.turnkey = 10 #turns after going from J to K
         self.forwardWires = "ESOVPZJAYQUIRHXLNFTGKDCMWB"
         self.backwardWires = "HZWVARTNLGUPXQCEJMBSKDYOIF"
 
 class RotorFive(Rotor):
     def __init__(self, orientation):
         super().__init__(orientation, 5)
-        self.turnkey = 1    #turns after going from Z to A
+        self.turnkey = 0    #turns after going from Z to A
         self.forwardWires = "VZBRGITYUPSDNHLXAWMJQOFECK"
         self.backwardWires = "QCYLXWENFTZOSMVJUDKGIARPHB"     
 
 
 #public static void main
-e = Enigma("ABCDEFGHIJKLMNOPQRST", '123', [12,3,21])
-print(e.scramble("The Funny Thing"))
+e = Enigma("ABCDEFGHIJKLMNOPQRST", '123', [12,5,22])
+print(e.rotors)
+e.shiftRotors()
+print(e.rotors)
+e.shiftRotors()
+print(e.rotors)
+e.shiftRotors()
+print(e.rotors)
 
 """
 All the letters:
@@ -207,27 +263,27 @@ All the letters:
 1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26
 A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
 
-Rotor One:
+Rotor One: Q to R
 Input:      A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
 Forward:    E   K   M   F   L   G   D   Q   V   Z   N   T   O   W   Y   H   X   U   S   P   A   I   B   R   C   J
 Back:       U   W   Y   G   A   D   F   P   V   Z   B   E   C   K   M   T   H   X   S   L   R   I   N   Q   O   J
 
-Rotor Two:
+Rotor Two: E to F
 Input:      A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
 Forward:    A   J   D   K   S   I   R   U   X   B   L   H   W   T   M   C   Q   G   Z   N   P   Y   F   V   O   E
 Back:       A   J   P   C   Z   W   R   L   F   B   D   K   O   T   Y   U   Q   G   E   N   H   X   M   I   V   S
 
-Rotor Three:
+Rotor Three:V to W
 Input:      A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
 Forward:    B   D   F   H   J   L   C   P   R   T   X   V   Z   N   Y   E   I   W   G   A   K   M   U   S   Q   O
 Back:       T   A   G   B   P   C   S   D   Q   E   U   F   V   N   Z   H   Y   I   X   J   W   L   R   K   O   M
 
-Rotor Four:
+Rotor Four: J to K
 Input:      A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
 Forward:    E   S   O   V   P   Z   J   A   Y   Q   U   I   R   H   X   L   N   F   T   G   K   D   C   M   W   B
 Back:       H   Z   W   V   A   R   T   N   L   G   U   P   X   Q   C   E   J   M   B   S   K   D   Y   O   I   F
 
-Rotor Five:
+Rotor Five: Z to A
 Input:      A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
 Forward:    V   Z   B   R   G   I   T   Y   U   P   S   D   N   H   L   X   A   W   M   J   Q   O   F   E   C   K
 Back:       Q   C   Y   L   X   W   E   N   F   T   Z   O   S   M   V   J   U   D   K   G   I   A   R   P   H   B
